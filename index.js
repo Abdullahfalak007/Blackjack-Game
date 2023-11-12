209f138ea766a6de7c49b32256631a0aba7175fd
Muhammad Abdullah <abdullahfalak007@gmail.com>
2023-11-12 15:09:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 7
 let secondCard = 6
+let cards = [firstCard, secondCard] 
 let sum = firstCard + secondCard
 let isAlive = true
 let blackJack = false
@@ -8,8 +9,20 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.innerHTML = "Cards: " + firstCard + " " + secondCard
+    // cardsEl.innerHTML = "Cards: " + cards[0] + " " + cards[1]
+    
+    cardsEl.innerHTML = "Cards: "
+    //Loop to concatenate the cards into the array.
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.innerHTML += cards[i] + " ";
+        
+    }
+    //Logic of the game
     if (sum <= 20)
     {
         message = "Do you want to draw another card?"
@@ -33,6 +46,9 @@ function newCard() {
     let card = 7
     // 2. Add the new card to the sum variable
     sum += card
+    //pusing the last card into the array
+    cards.push(card)
+
     // 3. Call startGame()
-    startGame()
+    renderGame()
 }
