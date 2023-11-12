981cf64beb2f5327ab1d7a3a700b2afbd667f98c
Muhammad Abdullah <abdullahfalak007@gmail.com>
2023-11-13 00:32:28 +0500

    New Card Function Fixed
    In this update, the new card button gets disable once you are out of the game or got a blackjack.

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-let firstCard = 7
-let secondCard = 6
-let cards = [firstCard, secondCard] 
-let sum = firstCard + secondCard
-let isAlive = true
+
+let cards = [] 
+let sum = 0
+let isAlive = false
 let blackJack = false
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+//player object
+let player = {
+    name: "Abdullah Falak",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function startGame() {
+    isAlive = true
+
+    // Generate two random numbers
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+
+    // Re-assign the cards and sum variables so that the game can start
+    cards.push(firstCard)
+    cards.push(secondCard)
+
+    //To sum the values
+    for (let i = 0; i < cards.length; i++) {
+        sum += cards[i]
+    }
+
     renderGame()
+}
+
+//function to generate the random number
+function getRandomCard() {
+    let randomNumber = Math.floor((Math.random() * 13) + 1)
+    
+    //Logic to change the values as below:
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function renderGame() {
@@ -41,14 +80,16 @@ function renderGame() {
 }
 
 function newCard() {
-    messageEl.textContent = "Drawing a new card from the deck!"
-    // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 7
-    // 2. Add the new card to the sum variable
-    sum += card
-    //pusing the last card into the array
-    cards.push(card)
+    if (isAlive === true && blackJack === false) {
+        messageEl.textContent = "Drawing a new card from the deck!"
+        // 1. Create a card variable, and hard code its value to a number (2-11)
+        let card = getRandomCard()
+        // 2. Add the new card to the sum variable
+        sum += card
+        //pusing the last card into the array
+        cards.push(card)
 
-    // 3. Call startGame()
-    renderGame()
+        // 3. Call startGame()
+        renderGame()
+    }
 }
